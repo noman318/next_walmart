68e504b6c36b8acd95a82adef114698ea7edddc4
Shaikh Noman <shaikhmohdnoman612@gmail.com>
2024-02-24 13:22:31 +0530

    Maintaining global store using zustand

--- walmart-2.0/components/AddToCart.tsx
+++ walmart-2.0/components/AddToCart.tsx
@@ -2,6 +2,7 @@ import { ProductContent } from "@/types/product";
 import React from "react";
 
 function AddToCart({ product }: { product: ProductContent }) {
+  //   console.log("product", product);
   return (
     <div>
       <button>Add to Cart</button>
